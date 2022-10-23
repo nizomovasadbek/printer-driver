db57d786e8dfa0935ac3433bbe83cc7724cd0715
nizomovasadbek <nizomovasadbekjava@gmail.com>
2022-10-23 10:17:33 +0500

    PrinterInfo added

--- src/main/java/uz/driver/commands/system/PrinterInfo.java
+++ src/main/java/uz/driver/commands/system/PrinterInfo.java
@@ -4,8 +4,26 @@ import lombok.Builder;
 import lombok.Data;
 import uz.driver.commands.TSPLCommand;
 
+import static uz.driver.DriverConstant.EMPTY_SPACE;
+import static uz.driver.DriverConstant.LF;
+import static uz.driver.commands.system.SystemCommand.SELFTEST;
+
 @Data
 @Builder
 public class PrinterInfo implements TSPLCommand {
     private PrinterInfoPage page;
+
+    @Override
+    public String getCommand() {
+        StringBuilder commandBuilder = new StringBuilder(SELFTEST.name());
+        if (page != null) {
+            commandBuilder.append(EMPTY_SPACE)
+                    .append(page.name());
+        }
+
+        commandBuilder.append(LF);
+
+        return commandBuilder.toString();
+    }
+
 }
